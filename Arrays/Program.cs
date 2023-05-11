b9e7379deb529bdaa0971d40552f572e22ce94d4
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-11 00:20:32 -0600

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// single quote identifies a char
+// double quotes identifies a string
+
+string data = "Tim,Sue,Bob,Jane,Frank";
+
+string[] firstNames = data.Split(',');
+
+foreach (string name in firstNames)
+{
+    Console.WriteLine(name);
+}
+
+string[] lastNames = new string[] { "Corey", "Smith", "Jones" };
+
+int[] ages = new int[] { 2, 3, 4 };
